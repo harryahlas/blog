fd62ef5a0f19973a6c55998ca11aa7160a3d198b
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-02-17 15:13:23 -0800

    Fixed updateURLs.R

--- scripts/updateURLs.R
+++ scripts/updateURLs.R
@@ -1,18 +1,19 @@
 setwd("C:\\Development\\github\\blog")
 
-
 blog_files <- list.files(getwd(), 
                          #pattern=(".html"),
                          full.names=F,
                          recursive = TRUE)
 
-local_url <- "C:\\\\Development\\\\blog\\\\harry\\\\public"
+# do not change this file or image files
+blog_files <- blog_files[blog_files != "scripts/updateURLs.R"]
+blog_files <- blog_files[grepl(blog_files,pattern = "/images/") != TRUE]
+
+
+# Make files for server ---------------------------------------------------
+
+local_url <- "C:\\\\Development\\\\github\\\\blog\\\\public"
 new_url <- "http://earlydemise.com/blog/public"
-
-local_url <- "http://earlydemise.com/blog"
-new_url <- "C:\\\\Development\\\\blog\\\\harry\\\\public"
-
-
 
 for(blog_file in blog_files) {
   x <- readLines(blog_file)
@@ -20,3 +21,13 @@ for(blog_file in blog_files) {
   cat(y, file=blog_file, sep="\n")
 }
 
+# Make files local --------------------------------------------------------
+
+server_url <- "http://earlydemise.com/blog/public"
+new_url <- "C:\\\\Development\\\\github\\\\blog\\\\public"
+
+for(blog_file in blog_files) {
+  x <- readLines(blog_file)
+  y <- gsub(server_url, new_url, x)
+  cat(y, file=blog_file, sep="\n")
+}
